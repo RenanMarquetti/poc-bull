1358e56961ea776e3c5a7f5f54b68ad29dbe8d0f
RenanMarquetti <codeum.desenvolvimento@gmail.com>
2025-05-10 12:34:03 -0300

    refactor: js to ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import Bull from 'bull';
-import JobSleep from './jobsleep.js';
+import JobSleep from './jobsleep.ts';
 
 const myFirstQueue = new Bull('my-first-queue');
 
 // producers
-myFirstQueue.add({timeSleep: 1000 * 5});
+myFirstQueue.add({timeSleep: 1});
 
 // consumers
 myFirstQueue.process(async (job) => {
-	return await new JobSleep(job.data.timeSleep).getSleepPromisse();
+	console.log("processing: ", job.data);
+	const j = new JobSleep(job.data.timeSleep);
+	const res = await j.getSleepPromisse();
+	console.log("res: ", res)
+	return res;
 });
 
 // listeners
